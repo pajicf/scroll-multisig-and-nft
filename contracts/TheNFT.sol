0514c486eed24bec7b517d911542740d5f09337e
Filip Pajic <fica.pajic@gmail.com>
2023-01-08 15:12:29 +0100

    Extend the NFT contract

--- contracts/TheNFT.sol
+++ contracts/TheNFT.sol
@@ -1,6 +1,18 @@
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Counters } from "@openzeppelin/contracts/utils/Counters.sol";
 
-contract TheNFT is ERC721 {
+contract TheNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
     constructor() ERC721("Scroll L2 NFT", "SNFT") {
+    }
+
+    function mint(address to) external onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _mint(to, tokenId);
     }
 }
